00fcbc1ce57982d1e0fbf324bdeba757725f7120
LeeSeokBong <kingstar6035@gmail.com>
2022-06-29 05:55:04 +0900

    코딩 테스트 연습

---
+++ 2022-06/20220628.js
@@ -0,0 +1,8 @@
+let input = require('fs').readFileSync('20220628.txt').toString().trim().split(" ");
+let num = Number(input[1] + input[0])
+
+let a = parseInt(input[0]);
+let b = parseInt(input[1]);
+console.log(a)
+console.log(b)
+console.log(a+b);
